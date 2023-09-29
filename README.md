58277b917c0b50d6a7be065c222f33dac9b3c023
Arison Firmino <arisonfirmino@hotmail.com>
2023-09-28 21:12:23 -0300

    readme added

---
+++ README.md
@@ -0,0 +1,21 @@
+<div>
+<img width="550" align="left" src="./images/preview.png"/>
+</div>
+
+<div>
+<h1 align="center">Stopwatch</h1>
+
+<p align="center">Projeto desenvolvido usando</p>
+
+<div align="center">
+<img width="200" src="./images/icon.png"/>
+</div>
+
+<hr>
+
+<p align="center">acesse a demo aqui 👇</p>
+<p align="center">
+<a href="https://arisonfirmino.github.io/flappy-superman/">arisonfirmino.github.io/stopwatch</a>
+</p>
+
+</div>
--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const timerDisplay = document.querySelector(".timer-display");
-const startButton = document.querySelector(".start-button");
-const pauseButton = document.querySelector(".pause-button");
-const resetButton = document.querySelector(".reset-button");
+const timerDisplay = document.querySelector(".display");
 
 let startTime = 0;
 let elapsedTime = 0;
@@ -11,35 +8,6 @@ let intervalId;
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
-
-startButton.addEventListener("click", () => {
-    if (paused) {
-        paused = false;
-        startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime, 1000);
-    }
-});
-
-pauseButton.addEventListener("click", () => {
-    if (!paused) {
-        paused = true;
-        elapsedTime = Date.now() - startTime;
-        clearInterval(intervalId);
-    }
-});
-
-resetButton.addEventListener("click", () => {
-    paused = true;
-    clearInterval(intervalId);
-    startTime = 0;
-    elapsedTime = 0;
-    currentTime = 0;
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-
-    timerDisplay.textContent = "00:00:00";
-});
 
 function updateTime() {
     elapsedTime = Date.now() - startTime;
@@ -58,3 +26,32 @@ function updateTime() {
         return ("0" + unit).length > 2 ? unit : "0" + unit;
     }
 }
+
+document.querySelector(".start-button").addEventListener("click", function () {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 1000);
+    }
+});
+
+document.querySelector(".pause-button").addEventListener("click", function () {
+    if (!paused) {
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+
+document.querySelector(".reset-button").addEventListener("click", function () {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    timerDisplay.textContent = "00:00:00";
+});
